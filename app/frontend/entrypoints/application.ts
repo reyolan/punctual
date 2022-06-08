82975484cc065dd4a4f0d63a68302b0f0c677408
reyolan <reynardolan@gmail.com>
2022-06-09 01:43:41 +0800

    first commit

---
+++ app/frontend/entrypoints/application.ts
@@ -0,0 +1,22 @@
+import ActiveStorage from "@rails/activestorage";
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import "../../assets/stylesheets/application.css";
+
+ActiveStorage.start();
+Rails.start();
+Turbolinks.start();
+
+console.log("Vite ⚡️ Rails");
+
+console.log(
+  "Visit the guide for more information: ",
+  "https://vite-ruby.netlify.app/guide/rails"
+);
+
+//
+// // Import all channels.
+// const channels = import.meta.globEager('./**/*_channel.js')
+
+// Example: Import a stylesheet in app/frontend/index.css
+// import '~/index.css'
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/helpers/**/*.rb",
+    "./app/frontend/entrypoints/*.ts",
+    "./app/views/**/*",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
