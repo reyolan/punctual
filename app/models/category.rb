5069a60188af35cf8ffdd24d3cd11552d542d5dd
reyolan <reynardolan@gmail.com>
2022-06-11 05:21:00 +0800

    edit design of views

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
   default_scope -> { order(:name) }
-  validates :name, uniqueness: { scope: :user_id }, presence: true
+  validates :name, uniqueness: { scope: :user_id }, presence: true, length: { maximum: 24 }
 end
