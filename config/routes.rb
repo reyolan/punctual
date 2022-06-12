d93f1742677872fce6528ecc3498493db9fc7ab0
reyolan <reynardolan@gmail.com>
2022-06-13 04:12:14 +0800

    finish all basic functionalities and design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[]
   resources :tasks, only: %i[new create]
   resources :categories do
     resources :tasks, shallow: true, except: :index
