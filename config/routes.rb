f8ba516ff90e6aedf3b778da0495c1a051b97638
reyolan <reynardolan@gmail.com>
2022-06-13 07:56:27 +0800

    add minor changes in category view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
-    get '/users/password', to: 'devise/passwords#new'
   end
   devise_for :users
 
