405cee12fe57da8030b5460fd91d9ce6073865c5
reyolan <reynardolan@gmail.com>
2022-06-13 06:56:17 +0800

    edit prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,18 +71,17 @@ Rails.application.configure do
 
   # Mail config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'punctually.herokuapp.com' }
+  host = 'punctually.herokuapp.com'
+  config.action_mailer.default_url_options = { host: }
 
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: 'apikey',
-    password: ENV['SENDGRID_API_KEY'],
-    domain: 'heroku.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     enable_starttls_auto: true
   }
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
