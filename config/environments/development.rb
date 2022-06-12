add634931e0b748c24f5eec4dcd328d10e0a6dee
reyolan <reynardolan@gmail.com>
2022-06-13 04:34:24 +0800

    edit e-mail messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 require "active_support/core_ext/integer/time"
 
+# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -34,6 +35,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Configuration for MailCatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
