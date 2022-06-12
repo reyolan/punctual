aa66fee4cb8f31d7082d061c234a9cbd9d8bf28b
reyolan <reynardolan@gmail.com>
2022-06-13 05:22:51 +0800

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
     get '/users/password', to: 'devise/passwords#new'
   end
+  devise_for :users
+
   resources :tasks, only: %i[new create]
   resources :categories do
     resources :tasks, shallow: true, except: :index
@@ -14,5 +15,4 @@ Rails.application.routes.draw do
   end
 
   root 'static_pages#home'
-  get '/settings', to: 'static_pages#settings', as: 'settings'
 end
