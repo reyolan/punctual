41daa1dc16aa81571927232a6b9785f4e52b4d93
reyolan <reynardolan@gmail.com>
2022-06-13 19:14:01 +0800

    add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User One
+
+user_one = User.create!(email: 'example@example.com',
+                        password: '1234567890',
+                        password_confirmation: '1234567890',
+                        confirmed_at: Time.now.utc)
+
+10.times do |n|
+  user_one.categories.create!(name: "Category-u1 #{n}")
+end
+
+category_one = user_one.categories.first
+
+10.times do |n|
+  category_one.tasks.create!(name: "Task-u1 #{n}", details: "Description-u1 #{n}", deadline: Date.current)
+end
+
+
+# User Two
+
+user_two = User.create!(email: '123re@example.com',
+                        password: '1234567890',
+                        password_confirmation: '1234567890',
+                        confirmed_at: Time.now.utc)
+10.times do |n|
+  user_two.categories.create!(name: "Category-u2 #{n}")
+end
+
+category_two = user_two.categories.first
+
+10.times do |n|
+  category_two.tasks.create!(name: "Task-u2 #{n}", details: "Description-u2 #{n}", deadline: Date.current)
+end
