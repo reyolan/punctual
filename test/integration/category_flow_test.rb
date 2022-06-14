2b6222c1752c1436efea65c5057d10003ce62bf1
reyolan <reynardolan@gmail.com>
2022-06-14 18:51:51 +0800

    merge integration test into their respective flow test file

--- test/integration/category_flow_test.rb
+++ test/integration/category_flow_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
-class CategoriesEditTest < ActionDispatch::IntegrationTest
+class CategoryFlowTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:mike)
     @first_category = @user.categories.first
+  end
+
+  test 'can create a category' do
+    get new_user_session_path
+    sign_in(@user)
+    get new_category_path
+    name = 'Example'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: } }
+    end
+    assert_not flash.empty?
+    follow_redirect!
+    assert_response :success
+    assert_select 'h1', name
+  end
+
+  test 'cannot create a category with invalid information' do
+    get new_user_session_path
+    sign_in(@user)
+    get new_category_path
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: 'a' * 25 } }
+    end
+    assert_select 'div#error_explanation'
   end
 
   test 'can edit a category' do
--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
-class UsersSignupTest < ActionDispatch::IntegrationTest
+class UserFlowTest < ActionDispatch::IntegrationTest
   def setup
+    @user = users(:mike)
     ActionMailer::Base.deliveries.clear
   end
 
@@ -24,5 +25,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                                      password_confirmation: 'exa' } }
     end
     assert_select 'div#error_explanation'
+  end
+
+  test 'login with valid email and password' do
+    get new_user_session_path
+    assert_select 'h2', 'Login'
+    sign_in(@user)
+    post user_session_path
+    assert user_logged_in?
+    assert_redirected_to authenticated_root_url
+    follow_redirect!
+    assert_response :success
+    assert_not flash.empty?
+    assert_select 'h1', "Welcome, #{get_email_username(@user.email)}!"
+  end
+
+  test 'login with invalid information' do
+    get new_user_session_path
+    assert_select 'h2', 'Login'
+    post user_session_path, params: { session: { email: ' ', password: ' ' } }
+    assert_not user_logged_in?
+    assert_not flash.empty?
+    assert_equal I18n.t('devise.failure.invalid', authentication_keys: :Email), flash.now[:alert]
   end
 end
