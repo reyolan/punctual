1c72108e8ab43656b69206e55f5b7111229409a9
reyolan <reynardolan@gmail.com>
2022-06-18 01:41:54 +0800

    refactor query_tasks method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   def query_tasks(model)
     @tasks_today = model.tasks.not_completed.where(deadline: Date.current)
-    nil_deadline_not_completed = model.tasks.not_completed.where(deadline: nil)
-    @tasks_not_completed = model.tasks.not_completed.where.not(deadline: Date.current).or(nil_deadline_not_completed)
+    @tasks_not_completed = model.tasks.not_completed.where.not(deadline: Date.current)
+                                .or(model.tasks.not_completed.where(deadline: nil))
     @tasks_completed = model.tasks.completed
   end
 
