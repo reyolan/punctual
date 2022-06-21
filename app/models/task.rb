d6fc0b5cfff05c2a982feb9b7713bd5843186755
reyolan <reynardolan@gmail.com>
2022-06-21 16:52:19 +0800

    remove eager load scoping

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,4 @@ class Task < ApplicationRecord
 
   scope :completed, -> { where(completed: true) }
   scope :not_completed, -> { where(completed: false) }
-  scope :eager_load_tasks, -> { tasks.includes(:category) }
 end
